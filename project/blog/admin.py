5badd25e4b292408626b7b7bb72f3aca9f055ac1
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-06-01 23:10:43 +0200

    Dnick Lab 3, Models and Admin

--- project/blog/admin.py
+++ project/blog/admin.py
@@ -1,3 +1,91 @@
-from django.contrib import admin
+from __future__ import annotations
 
-# Register your models here.
+from django.contrib import admin
+from django.forms import ModelForm
+from django.http import HttpRequest
+from django.contrib.admin import DateFieldListFilter
+
+from .models import BlogUser, Post, Comment, File, Block
+
+
+@admin.register(BlogUser)
+class BlogUserAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        if obj and request.user == obj.user:
+            return True
+        return False
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "user"]
+    search_fields = ["title", "content"]
+    list_filter = (
+        ('creation_date', DateFieldListFilter),
+    )
+    exclude = ["user"]
+
+    def has_change_permission(self, request, obj=None):
+        # obj.user is our custom BlogUser, obj.user.user is the Django out of the box user
+        if obj and request.user == obj.user.user:
+            return True
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        if obj:
+            print(obj.user)  # BlogUser
+            print(obj.user.user)  # user
+            print(request.user)  # user
+
+        if request.user.is_superuser or (
+                obj and not Block.objects.filter(blocker__user=obj.user.user, blocked__user=request.user).exists()):
+            return True
+        return False
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["content", "creation_date"]
+    exclude = ["user"]
+
+    def has_view_permission(self, request, obj=None):
+        if request.user.is_superuser or (
+                obj and not Block.objects.filter(blocker__user=obj.user.user, blocked__user=request.user).exists()):
+            return True
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        # obj.user is our custom BlogUser (Comment model has a User ForeignKey)
+        # obj.user.user is the Django out of the box user
+        if obj and request.user == obj.user.user:
+            return True
+        return False
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+
+    def has_view_permission(self, request, obj=None):
+        if request.user.is_superuser or (
+                obj and not Block.objects.filter(blocker__user=obj.user.user, blocked__user=request.user).exists()):
+            return True
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if obj and request.user == obj.user.user:
+            return True
+        return False
+
+
+@admin.register(Block)
+class BlockAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        if obj and request.user == obj.blocker.user:
+            return True
+        return False
